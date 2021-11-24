a33d2e11ebac694dc8fd8459a51731ac940fa72d
Kyle Coffey <mariorules24@gmail.com>
2021-11-24 08:59:07 -0600

    use _Generic macros

--- inc/helpers/math.h
+++ inc/helpers/math.h
@@ -2,3 +2,6 @@
 
 int math_map_i(int value, int input_min, int input_max, int output_min, int output_max);
 double math_map_d(double value, double input_min, double input_max, double output_min, double output_max);
+
+#define math_map(value, input_min, input_max, output_min, output_max)                                                  \
+    _Generic((value), int : math_map_i, double : math_map_d)(value, input_min, input_max, output_min, output_max)
--- inc/helpers/random.h
+++ inc/helpers/random.h
@@ -2,3 +2,5 @@
 
 int rand_range_i(int min, int max);
 double rand_range_d(double min, double max);
+
+#define rand_range(min, max) _Generic((min), int : rand_range_i, double : rand_range_d)((min), (max))
