c7d1fb10ba44a69a92c5947a1b5b7ef11ddb76a6
Kyle Coffey <mariorules24@gmail.com>
2021-11-24 08:34:48 -0600

    Add helper library

---
+++ inc/helpers/helpers.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <SDL.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+#define HELPERS_INIT(Title, Width, Height, WindowName, RendererName)                                                   \
+    do                                                                                                                 \
+    {                                                                                                                  \
+        if (SDL_Init(SDL_INIT_VIDEO) != 0)                                                                             \
+        {                                                                                                              \
+            printf("SDL_Init Error: %s\n", SDL_GetError());                                                            \
+            return 1;                                                                                                  \
+        }                                                                                                              \
+        WindowName = SDL_CreateWindow(Title, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, Width, Height,          \
+                                      SDL_WINDOW_SHOWN);                                                               \
+        if (window == NULL)                                                                                            \
+        {                                                                                                              \
+            printf("SDL_CreateWindow Error: %s\n", SDL_GetError());                                                    \
+            SDL_Quit();                                                                                                \
+            return 1;                                                                                                  \
+        }                                                                                                              \
+        RendererName = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);           \
+        if (renderer == NULL)                                                                                          \
+        {                                                                                                              \
+            printf("SDL_CreateRenderer Error: %s\n", SDL_GetError());                                                  \
+            SDL_DestroyWindow(window);                                                                                 \
+            SDL_Quit();                                                                                                \
+            return 1;                                                                                                  \
+        }                                                                                                              \
+    } while (false)
+
+#define HELPERS_CLEANUP()                                                                                              \
+    do                                                                                                                 \
+    {                                                                                                                  \
+        SDL_DestroyRenderer(renderer);                                                                                 \
+        SDL_DestroyWindow(window);                                                                                     \
+        SDL_Quit();                                                                                                    \
+    } while (false)
--- src/c001_starfield/main.c
+++ src/c001_starfield/main.c
@@ -1,6 +1,11 @@
-#include <SDL.h>
+#include <helpers/helpers.h>
 
 int main(int argc, char** argv)
 {
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    HELPERS_INIT("Starfield", 800, 600, window, renderer);
+
+    HELPERS_CLEANUP();
     return 0;
 }
