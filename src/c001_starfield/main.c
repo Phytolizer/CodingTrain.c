ab1ebb095dafaf79f684efff844e4f7f3201b3d2
Kyle Coffey <mariorules24@gmail.com>
2021-11-24 08:36:58 -0600

    keep the window alive

--- src/c001_starfield/main.c
+++ src/c001_starfield/main.c
@@ -6,6 +6,27 @@ int main(int argc, char** argv)
     SDL_Renderer* renderer;
     HELPERS_INIT("Starfield", 800, 600, window, renderer);
 
+    bool quit = false;
+    while (!quit)
+    {
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            switch (event.type)
+            {
+            case SDL_QUIT:
+                quit = true;
+                break;
+            default:
+                break;
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+    }
+
     HELPERS_CLEANUP();
     return 0;
 }
